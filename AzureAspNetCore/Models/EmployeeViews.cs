4ad2f23da740793145e38ce6b53ae3e9a1baf8f9
Дмитрий Горбовский <dimon007@inbox.ru>
2020-06-09 17:45:20 +0300

    Первая заливка проекта, настройка MVC + создание списка Employees

---
+++ AzureAspNetCore/Models/EmployeeViews.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAspNetCore.Models
+{
+    public class EmployeeViews
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public short Age { get; set; }
+
+        public EmployeeViews(string firstName, string lastName, short age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
