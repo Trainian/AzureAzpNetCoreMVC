8f9d759df1dac531bb097fa2fe865f08da68639d
Дмитрий Горбовский <dimon007@inbox.ru>
2020-06-17 08:36:27 +0300

    Save

---
+++ AzureAspNetCore/Models/BrandView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureAspNetCore.Domain.Entities.Base;
+
+namespace AzureAspNetCore.Models
+{
+    public class BrandView : OrderedEntity
+    {
+
+    }
+}
---
+++ AzureAspNetCore/ViewComponents/BrandViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureAspNetCore.Infrastructure.Interfaces;
+using AzureAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAspNetCore.ViewComponents
+{
+    public class BrandViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+
+        public BrandViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var brends = GetBrands();
+            return View(brends);
+        }
+
+        private List<BrandView> GetBrands()
+        {
+            var brands = _productData.GetBrands();
+            var listBrands = new List<BrandView>();
+            foreach (var brand in brands)
+            {
+                listBrands.Add(new BrandView()
+                {
+                    Id = brand.Id,
+                    Name = brand.Name,
+                    Order = brand.Order
+                });
+            }
+
+            listBrands.OrderBy(e => e.Id).ToList();
+            return listBrands;
+        }
+    }
+}
