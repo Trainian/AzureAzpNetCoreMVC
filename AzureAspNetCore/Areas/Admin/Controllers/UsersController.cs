cf39e2f05c7c7a5be236deb229e6ee70900c0cdd
Дмитрий Горбовский <dimon007@inbox.ru>
2020-07-22 17:09:44 +0300

    Save

---
+++ AzureAspNetCore/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureAspNetCore.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAspNetCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
---
+++ AzureAspNetCore/Areas/Admin/Models/UserView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAspNetCore.Areas.Admin.Models
+{
+    public class UserView
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Имя не может быть пустым")]
+        [DisplayName("Пользователь")]
+        public string UserName { get; set; }
+
+        [DisplayName("Почта")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DisplayName("Телефон")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("Роль")]
+        public string Role { get; set; }
+    }
+}
